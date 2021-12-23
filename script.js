33157492a9b7d5fc23340caf34cd4702784f9350
Tommy <tommychabiras@gmail.com>
2021-12-23 02:15:58 -0500

    added ternary conditional on button for display on and off

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let myLibrary = [];
+const inputForm =  document.getElementById("input-form");
 const button = document.querySelector("button");
 button.addEventListener("click", (e) => {
+    inputForm.style.display == "none" ? inputForm.style.display = "flex" : inputForm.style.display = "none";
     bookName = 
     author = 
     pages = 
     read = 
-    addBookToLibrary(bookName, author, pages, read
+    console.log("aioweg");
+    addBookToLibrary(bookName, author, pages, read)
 });
 
 
