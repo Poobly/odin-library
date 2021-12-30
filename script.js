c5abb9977785a32c194f46846d498c46731ed3eb
Tommy <tommychabiras@gmail.com>
2021-12-30 01:49:38 -0500

    added create cards function and display books function which displays those cards

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const inputForm =  document.getElementById("input-form-modal");
 const newBook = document.getElementById("new-book");
 const saveBook = document.getElementById("save-book");
 const bookCon = document.getElementById("book-con");
+const inputModal = document.getElementById("input-form-modal");
+
 newBook.addEventListener("click", (e) => {
     inputForm.style.display = "flex"
+});
+
+inputModal.addEventListener("mousedown", (e) => {
+    if (e.target === e.currentTarget) {
+        saveBook.click();
+    }
 });
 
 saveBook.addEventListener("click", (e) => {
@@ -12,9 +20,12 @@ saveBook.addEventListener("click", (e) => {
     let bookName = document.getElementById("bookName").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
-    addBookToLibrary(bookName, author, pages, read)
-    displayBooks();
+    let read = document.getElementById("read").checked;
+    bookName = addBookToLibrary(bookName, author, pages, read);
+    createCards(bookName);
+    document.querySelectorAll("#input-form-con > input").forEach((input) => {
+        input.type == "checkbox" ? input.checked = false : input.value = ""; 
+    });
 });
 
 
@@ -22,24 +33,78 @@ function Book(bookName, author, pages, read) {
     this.bookName = bookName;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = getRead(read, bookName);
+};
+
+function getRead(read, bookName) {
+    let re = /^[ ]+$/g;
+    if (re.test(bookName) || bookName === "") bookName = "N/A";
+    return read == true ? `${bookName} has been read` : `${bookName} has not been read`;
 }
+
 
 function addBookToLibrary(bookName, author, pages, read) {
+    console.log(bookName);
     bookName = new Book(bookName, author, pages, read);
     myLibrary.push(bookName);
+    return bookName;
 }
 
-function displayBooks() {
+function createCards(bookName) {
+    const card = document.createElement("div");
+    card.className = "card-con";
+    bookCon.appendChild(card);
+
+    const buttonDiv = document.createElement("div");
+    buttonDiv.id = "card-botton-con";
+    card.appendChild(buttonDiv);
+
+    const editButton = document.createElement("button");
+    editButton.className = "edit-button";
+    editButton.classList.add("card-bottons")
+    editButton.textContent = "Edit";
+    // editButton.addEventListener("click", )
+    buttonDiv.appendChild(editButton);
+    
+    const delButton = document.createElement("button");
+    delButton.className = "delete-button";
+    delButton.classList.add("card-bottons")
+    delButton.textContent = "Close";
+    buttonDiv.appendChild(delButton);
+
     const list = document.createElement("ul");
-    list.classList.add("card");
-    bookCon.appendChild(list);
-    for (const book of myLibrary) {
-        Object.keys(book).forEach(value => {
-            const listEl = document.createElement("li");
-            list.appendChild(listEl);
-            listEl.textContent = book[value];
-        });
+    list.className = "card";
+    card.appendChild(list);
+
+    displayBooks(bookName, list);
+}
+
+function displayBooks(bookName, list) {
+    for (value in bookName) {
+        let re = /^[ ]+$/g;
+        const listEl = document.createElement("li");
+        list.appendChild(listEl);
+        console.log(bookName);
+        if (re.test(bookName[value]) || bookName[value] === "") {
+            bookName[value] = "N/A";
+        }
+        switch (value) {
+            case "bookName": 
+                listEl.textContent = `Book Name: ${bookName[value]}`;
+                break;
+            case "author":
+                listEl.textContent = `Author: ${bookName[value]}`;
+                break;
+            case "pages":
+                listEl.textContent = `Pages read: ${bookName[value]}`;
+                break;
+            case "read":
+                if (bookName[value] === "N/A") {
+                    console.log("apple")
+                }
+                listEl.textContent = bookName[value];
+                break;
+        }
     }
 }
 
